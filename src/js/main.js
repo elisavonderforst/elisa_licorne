857a9faa79f757215418c0fb4985016e6806f70d
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2024-06-18 16:13:39 +0200

    menu hidden pas fini
    je pete un cable

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenuButton = document.querySelector(".menu-button");
+  const profilMenu = document.querySelector(".header-profil-and-shop");
+  let lastScrollTop = 0;
+
+  window.addEventListener("scroll", function () {
+    const currentScrollTop =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScrollTop > lastScrollTop) {
+      burgerMenuButton.classList.add("hidden-scroll");
+      profilMenu.classList.add("hidden-scroll");
+    } else {
+      burgerMenuButton.classList.remove("hidden-scroll");
+      profilMenu.classList.remove("hidden-scroll");
+    }
+
+    lastScrollTop = currentScrollTop;
+  });
+});
 
 document.addEventListener("scroll", function () {
   const scrollImage = document.getElementById("headerScrollImg");
@@ -10,3 +30,15 @@ document.addEventListener("scroll", function () {
     scrollImage.style.top = 50 + scrollLimit + "px";
   }
 });
+
+var openMenu = document.querySelector(".menu-button");
+var closeMenu = document.querySelector(".menu-button-closemenu");
+var blockMenu = document.querySelector(".menu-hidden");
+var burgerMenu = document.getElementById("burgerMenu");
+
+var openCloseMenu = function openCloseMenu() {
+  blockMenu.classList.toggle("is-open");
+};
+
+openMenu.addEventListener("click", openCloseMenu);
+closeMenu.addEventListener("click", openCloseMenu);
